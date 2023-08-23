744a5d56c8bca925ddea18c2e6adc9df0dc8c467
Galip Uğur <ugurgalip30@gmail.com>
2023-08-23 11:25:51 +0300

    explanation added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from scipy.io.wavfile import write
 
 fre = 44100
 
+# you can set the duration of the recording yourself
 second = int(input("Enter time duration in seconds: "))
 
 recordVoice = sounddevice.rec(int(second * fre), samplerate= fre, channels= 2)
